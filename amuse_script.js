1c5acdb9776c4019378fb54fdf1ab83f9e920491
jamaripeterson24 <jamaripeterson24@icloud.com>
2026-05-04 23:18:32 -0400

    Refactor showRides function for cleaner code

--- amuse_script.js
+++ amuse_script.js
@@ -1,26 +1,15 @@
 function rideInfo(rideID, rideName) {
     alert("You clicked on the ride with the ID of: " + rideID + " and name of: " + rideName);
 }
+
 function showRides() {
-   function showRides() {
-
-    const rideNames = [
-        "Soar Over Paulson", 
-        "Gus's Flight School", 
-        "Splashin the Boro"
-    ];
-
+    const rideNames = ["Soar Over Paulson", "Gus's Flight School", "Splashin the Boro"];
     const rideDescriptions = [
         "A 200-foot vertical drop coaster that gives you an amazing site of our beloved Paulson stadium.", 
         "A high-speed spinning thrill ride that makes you feel like an eagle.", 
         "A water adventure perfect for summer here in the boro."
     ];
-
-    const rideImages = [
-        "Soaring.jpeg.jpg", 
-        "Gus.jpeg.jpg", 
-        "Watercoaster.jpeg.jpg"
-    ];
+    const rideImages = ["Soaring.jpeg.jpg", "Gus.jpeg.jpg", "Watercoaster.jpeg.jpg"];
 
     let content = "";
 
@@ -36,10 +25,11 @@ function showRides() {
     document.getElementById("ride-container").innerHTML = content;
 }
 
-
+// This checks if the container exists before running the function
 if (document.getElementById("ride-container")) {
     showRides();
 }
+
 function orderTics() {
     alert("Your total price is: $35");
 }
